d3d38ee9798824f6d5440dce42ab9c3004a585da
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-02 12:26:15 +0300

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Create class BasicCache"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Implement BasicCache class"""
+
+    def __init__(self):
+        """Constructor method"""
+        super().__init__()
+        print(self.cache_data)
+
+    def put(self, key, item):
+        """append key item pair to dict"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return value in self.cache_data"""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
