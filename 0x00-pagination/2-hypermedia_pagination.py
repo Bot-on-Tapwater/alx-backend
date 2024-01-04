4a506134a482d1f18da8cdcb8c2db5a7a5486a0b
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-04 12:02:18 +0300

    Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Implement simple pagination"""
+
+import csv
+import math
+from typing import List, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        indexes = index_range(page, page_size)
+        return self.dataset()[indexes[0]:indexes[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Implement get_hyper method"""
+        data = self.get_page(page, page_size)
+        if (len(self.dataset()) % page_size == 0):
+            total_pages = len(self.dataset()) // page_size
+        else:
+            total_pages = len(self.dataset()) // page_size + 1
+        if page < total_pages:
+            next_page = page + 1
+        else:
+            next_page = None
+        if page != 1:
+            prev_page = page - 1
+        else:
+            prev_page = None
+        if page > total_pages:
+            page_size = 0
+
+        return {'page_size': page_size, 'page': page, 'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page, 'total_pages': total_pages}
+
+
+def index_range(page, page_size):
+    """Return tuple containing start and end index"""
+    return ((page - 1) * page_size, (page * page_size))
